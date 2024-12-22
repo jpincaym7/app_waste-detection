2be61057c30e2a4e89e3cc035ff470fb8874b06a
jpincaym7 <jpincaym7@unemi.edu.ec>
2024-12-22 03:26:58 -0500

    app_web_movil_settings

--- waste_detection/settings.py
+++ waste_detection/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
