57f459c2b5c891339b6505be9c9e55b00b6f4ff6
Jordy David Pincay Murillo <119023862+jpincaym7@users.noreply.github.com>
2025-01-12 03:55:32 -0500

    Update storage_backends.py

--- waste_detection/storage_backends.py
+++ waste_detection/storage_backends.py
@@ -2,11 +2,6 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
-class StaticStorage(S3Boto3Storage):
-    location = settings.STATIC_LOCATION
-    default_acl = 'public-read'
-    file_overwrite = True
-
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIA_LOCATION
     default_acl = 'public-read'
