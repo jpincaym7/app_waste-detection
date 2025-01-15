ee77bd17ad981e96a60e10e0e19cf268785ab303
Jordy David Pincay Murillo <119023862+jpincaym7@users.noreply.github.com>
2025-01-14 22:30:44 -0500

    Update urls.py

--- apps/waste/urls.py
+++ apps/waste/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.waste.views.capture import  WasteDetectionView
+from apps.waste.views.capture import  UpdateProgressView, WasteDetectionView
 
 app_name = 'waste'
 
 urlpatterns = [
     path('capture/', WasteDetectionView.as_view(), name="capture"),
+    path('api/update-progress/', UpdateProgressView.as_view(), name='update_progress'),
 ]
