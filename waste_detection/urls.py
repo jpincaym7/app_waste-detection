affa16d6ab54da802d9fc6d23ccb7b402ba12fc1
Jordy David Pincay Murillo <119023862+jpincaym7@users.noreply.github.com>
2025-01-25 16:10:41 -0500

    Update urls.py

--- waste_detection/urls.py
+++ waste_detection/urls.py
@@ -29,4 +29,4 @@ urlpatterns = [
     path("", HomeView.as_view(), name="home")
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL)
